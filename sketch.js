965a156ceb05b10e5d91a4a9e28eaf8343eec617
Vihan Perera <vihanpereraux@gmail.com>
2023-03-26 14:41:01 +0530

    Update curve vertex

--- sketch.js
+++ sketch.js
@@ -1,46 +1,90 @@
 var xoff = 0;
+var randomAngle;
+var d;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(25);
+  background(255, 255, 255);
+
+  angleMode(DEGREES);
+  randomAngle = random(1, 90);
+  d = random(100, 120);
 
   stroke(255);
   strokeWeight(8);
-  point(windowWidth/2, 0);
-  point(windowWidth/4, windowHeight/5);
-  point(0, windowHeight/2);
-  point(windowWidth/4, windowHeight*0.75);
-  point(windowWidth/2, windowHeight);
-  point(windowWidth, windowHeight/2);
+  createPoints(windowWidth, windowHeight, d);
+
+  // noFill();
+  // strokeWeight(.5);
+  // createShape(windowWidth, windowHeight, d);
 }
 
-// function draw() {
-//   noErase();
-//   noFill();
-  
-//   frameRate(2);
+function draw() {
+  frameRate(20);
 
-//   stroke(255);
-//   strokeWeight(0.1);
-  
-  
+  randomAngle = random(1, 90);
+  d = random(100, 120);
 
-//   beginShape();
-//     // curveVertex(windowWidth/2, 0);
-//     // curveVertex(windowWidth/2, 0);
+  noFill();
+  strokeWeight(.1);
+  stroke(150);
+  createShape(windowWidth, windowHeight, d);
+}
 
-//     // curveVertex(00, windowHeight/2);
+function createPoints(width, height, d){
+  point(width*.45, height*.15);
 
-//     // curveVertex(50, windowHeight/2);
+  point(width/4 + d*cos(randomAngle), windowHeight/5 - d*sin(randomAngle));
 
-//     // curveVertex(windowWidth/2, windowHeight);
+  point(width/4, height/5);
 
-//     // curveVertex(windowWidth, windowHeight/2);
+  point(width*.3, height*.4);
 
-//     // curveVertex(windowWidth/2, 0);
-//     // curveVertex(windowWidth/2, 0);
-//   endShape();
+  point(width/4 - d*cos(randomAngle), height*0.75 - d*sin(randomAngle));
 
-//   // fill(255, 0, 0);
-//   // circle(mouseX, mouseY, 100);
-// }
+  point(width/4, height*0.75);
+
+  point(width*.4, height*.95);
+
+  point(width*.57 - (d/2)*cos(randomAngle), height*0.75 + (d/2)*sin(randomAngle));
+
+  point(width*.65, height*.75);
+
+  point(width*.6, height/2);
+
+  point(width*.65, height*.3);
+
+  point(width*.57 - (d/2)*cos(randomAngle), height*.3 - d*sin(randomAngle));
+}
+
+function createShape(width, height, d){
+  beginShape();
+    curveVertex(width*.45, height*.15);
+    curveVertex(width*.45, height*.15);
+
+    curveVertex(width/4 + d*cos(randomAngle), height/5 - d*sin(randomAngle));
+
+    curveVertex(width/4, height/5);
+
+    curveVertex(width*.3, height*.4);
+
+    curveVertex(width/4 - d*cos(randomAngle), height*0.75 - d*sin(randomAngle));
+
+    curveVertex(width/4, height*0.75);
+
+    curveVertex(width*.4, height*.95);
+
+    curveVertex(width*.57 - (d/2)*cos(randomAngle), height*0.75 + (d/2)*sin(randomAngle));
+
+    curveVertex(width*.65, height*.75);
+
+    curveVertex(width*.6, height/2);
+
+    curveVertex(width*.65, height*.3);
+
+    curveVertex(width*.57 - (d/2)*cos(randomAngle), height*.3 - d*sin(randomAngle));
+
+    curveVertex(width*.45, height*.15);
+    curveVertex(width*.45, height*.15);
+  endShape();
+}
